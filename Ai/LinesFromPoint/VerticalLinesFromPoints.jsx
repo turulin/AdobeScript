59b424da3d21550a37e80f47e7b20fca4699d0ec
turulin <turulin@artlebedev.ru>
2021-02-19 23:17:49 +0300

    Fix points

--- Ai/LinesFromPoint/VerticalLinesFromPoints.jsx
+++ Ai/LinesFromPoint/VerticalLinesFromPoints.jsx
@@ -1,6 +1,6 @@
 /**
  * Copyright Sergey Turulin
- * https://adobescript.ru
+ * https://adobescript.ru/script
  * sergey@adobescript.ru
  */
 
@@ -32,12 +32,15 @@ for (var k = 0; k < selection.length; k++) {
   var
       pathItem = selection[k],
       i, point,
+      getRight, getLeft,
       anchor, leftDirection, rightDirection,
       dX, dY, points, angle, newAngle, line,
       selectedCount = 0;
 
   for (i = 0; i < pathItem.pathPoints.length; i++) {
-    point = pathItem.pathPoints[i];
+    getLeft = false
+    getRight = false
+    point = pathItem.pathPoints[i]
 
     if (PathPointSelection.ANCHORPOINT !== point.selected) {
       continue;
@@ -48,8 +51,24 @@ for (var k = 0; k < selection.length; k++) {
     leftDirection = roundPoint(pathItem.pathPoints[i].leftDirection);
     rightDirection = roundPoint(pathItem.pathPoints[i].rightDirection);
 
-    dX = anchor[0] - leftDirection[0];
-    dY = anchor[1] - leftDirection[1];
+    if (leftDirection[0] === anchor[0] && leftDirection[1] === anchor[1]) {
+      getRight = true
+    } else {
+      if (rightDirection[0] === anchor[0] && rightDirection[1] === anchor[1]) {
+        getLeft = true
+      } else {
+        continue
+      }
+    }
+
+    if (getLeft) {
+      dX = anchor[0] - leftDirection[0]
+      dY = anchor[1] - leftDirection[1]
+    } else {
+      dX = anchor[0] - rightDirection[0]
+      dY = anchor[1] - rightDirection[1]
+    }
+
     if (0 === dY) {
       angle = 0;
     } else {
